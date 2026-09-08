7bb9941a7da86653b3750f07cc38cddbbb49943a
vlfwww <vlfwww@outlook.com>
2026-09-08 13:58:22 +0300

    chore: fix webpack mode and babel react runtime for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,65 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.jsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.[contenthash].js",
-    clean: true,
-    publicPath: "/crowdfunding-spa/",
-  },
-  mode: "development",
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, "src"),
-        type: "javascript/auto",
-        use: {
-          loader: "babel-loader",
-          options: {
-            sourceType: "unambiguous",
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-            ],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[contenthash].js",
+      clean: true,
+      publicPath: "/crowdfunding-spa/",
+    },
+    mode: isProduction ? "production" : "development",
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: path.resolve(__dirname, "src"),
+          type: "javascript/auto",
+          use: {
+            loader: "babel-loader",
+            options: {
+              sourceType: "unambiguous",
+              presets: [
+                "@babel/preset-env",
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                    development: !isProduction,
+                  },
+                ],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|avif|webp)$/i,
-        type: "asset/resource",
-      },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|avif|webp)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+  };
 };
