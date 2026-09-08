359bf0688b149e059e2aceaf54643979193cb021
vlfwww <vlfwww@outlook.com>
2026-09-08 11:27:22 +0300

    fix: change footer position

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import "./Layout.css";
 
 const Layout = () => {
   return (
