476c5ad4b104bcfbbb6ab2ae9f71c6754284859a
vlfwww <vlfwww@outlook.com>
2026-09-09 10:13:39 +0300

    fix: change path to fix server error

--- src/store/api/shopApi.js
+++ src/store/api/shopApi.js
@@ -1,12 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const shopApi = createApi({
   reducerPath: "shopApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/c/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: isProd ? "https://dummyjson.com/c/" : "/c/",
+  }),
   endpoints: (builder) => ({
     getFields: builder.query({
       query: () => "4730-2d6a-40ed-b0eb",
-
       transformResponse: (response) => response.fields,
     }),
 
